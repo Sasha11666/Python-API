1c04b1e1368504d4f9419bd4314741e69e741cca
Sasha11666 <basovasasa087@gmail.com>
2024-10-02 09:18:22 -0400

    more corrections

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-# The API request to get the data (breeds) from the cats database:
+# The API request to get the data (breeds) from the cats database
 
 import requests
 response = requests.get("https://api.thecatapi.com/v1/breeds")
